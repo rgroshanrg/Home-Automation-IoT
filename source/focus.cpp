f4f0ace04b137a0520c766dace01db8737c0db8f
rgroshanrg <rgroshanrg@gmail.com>
2020-06-06 09:11:56 +0530

    Home Automation

---
+++ source/focus.cpp
@@ -0,0 +1,21 @@
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "******************";
+// Wifi Credentials
+char ssid[] = "**Wifi ssid name**";
+char pass[] = "wifi pass";
+
+void setup() {
+	//Debug Console
+	Serial.begin(9600);
+	
+	Blynk.begin(auth, ssid, pass);
+}
+
+void loop() {
+	//loop control
+	Blynk.run();
+}
